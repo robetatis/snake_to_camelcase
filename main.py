02bee8326e62c3c55186703172c895ffd00a81d7
roberto.tatis <robetatis@yahoo.com>
2023-07-19 12:37:41 +0200

    add README.md

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     input_string = '{"first_name": "John", "last_name": "Doe"}'
     converter = SnakeToCamelCase(input_string)
     converter.run()
-    print(input_string)
-    print(converter.string_camel)
+    print('input string:', input_string)
+    print('output string:', converter.string_camel)
 
